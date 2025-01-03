43c57ec9a4f300f9fdb48c37fa42b3bc501dc334
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-01-03 21:29:36 +0600

    adding animal class

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 
+
+
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      console.log(`${this.name} makes a sound.`);
+    }
+  }
+  
+  class Dog extends Animal {
+    speak() {
+      console.log(`${this.name} barks.`);
+    }
+  }
+  
+  const dog = new Dog("Rex");
+  dog.speak(); // Outputs: Rex barks.
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const ddtest='commit test'
 
 
