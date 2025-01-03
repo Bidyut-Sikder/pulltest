64526a27363d90e42f22d0b5edeb4322afe5880b
Bidyut Sikder <89535239+Bidyut-Sikder@users.noreply.github.com>
2025-01-03 21:28:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 
+
+
+
+
+
+class ClassName {
+  constructor(param1, param2) {
+    // Initialize properties
+    this.prop1 = param1;
+    this.prop2 = param2;
+  }
+
+  // Method
+  method1() {
+    console.log(`Property 1: ${this.prop1}`);
+  }
+
+  // Static method
+  static staticMethod() {
+    console.log("This is a static method.");
+  }
+}
+
+// Create an instance
+const obj = new ClassName("Value1", "Value2");
+obj.method1(); // Outputs: Property 1: Value1
+
+// Access static method
+ClassName.staticMethod(); // Outputs: This is a static method.
+
+
+
+
+
+
 const ddtest='commit test'
 
 
